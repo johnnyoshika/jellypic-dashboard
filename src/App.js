eb7c117dcf28d82fcb03c8a6df7be38a69bd1962
Johnny Oshika <johnnyoshika@gmail.com>
2018-02-14 08:41:35 -0800

    Use React Router.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Settings from './Settings';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <div>
+              <Link to="/">Home</Link>
+              |
+              <Link to="/settings">Settings</Link>
+            </div>
+          </header>
+          <div className="App-intro">
+            <Route exact path="/" component={Home} />
+            <Route path="/settings" component={Settings} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
