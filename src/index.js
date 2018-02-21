0fe6fdf8fb5d3561edf0c0c0f07fb4b600cf8be1
Johnny Oshika <johnnyoshika@gmail.com>
2018-02-21 08:49:47 -0800

    Configure store.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
+
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('/sw.js')
