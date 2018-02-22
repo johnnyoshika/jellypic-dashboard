fafca0c5d1d496d0791d1005040d845163c34956
Johnny Oshika <johnnyoshika@gmail.com>
2018-02-22 00:12:07 -0800

    Add toastr.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Session from './routes/Session'
 import Login from './routes/Login'
+import ReduxToastr from 'react-redux-toastr'
+import './Toastr.css'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/" component={Session} />
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/" component={Session} />
+            </Switch>
+          </div>
+        </Router>
+        <ReduxToastr
+          timeOut={6000}
+          newestOnTop={false}
+          preventDuplicates
+          position="top-right"
+          transitionIn="bounceIn"
+          transitionOut="bounceOut"
+          progressBar={false} />
+      </div>
     )
   }
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import session from './sessionReducer'
 import login from './loginReducer'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 const rootReducer = combineReducers({
   session,
-  login
+  login,
+  toastr: toastrReducer
 })
 
 export default rootReducer
