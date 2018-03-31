7b726a8ef36bfebc8105494c9d6d4fc7b8da46a8
Johnny Oshika <johnnyoshika@gmail.com>
2018-03-30 19:47:16 -0700

    Revert 'Add Refresher to Login.'

--- src/routes/Login/View.js
+++ src/routes/Login/View.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Refresher from '../../components/Refresher';
 import ErrorMessage from '../../components/ErrorMessage';
 import './Styles.css';
 
@@ -21,28 +20,25 @@ class View extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <Refresher />
-        <div className="login-container">
-          <div className="gutter" />
-          <div className="login-main">
-            <div className="font-lobster text-center mb-40">Jellypic</div>
-            <div className="text-center">
-              <button
-                className="btn btn-primary btn-lg"
-                disabled={this.disabled()}
-                onClick={() => this.props.loginWithFacebook()}
-              >
-                Log in with Facebook
-              </button>
-              {this.props.login.state === 'error' && (
-                <ErrorMessage message={this.props.login.error} />
-              )}
-            </div>
+      <div className="login-container">
+        <div className="gutter" />
+        <div className="login-main">
+          <div className="font-lobster text-center mb-40">Jellypic</div>
+          <div className="text-center">
+            <button
+              className="btn btn-primary btn-lg"
+              disabled={this.disabled()}
+              onClick={() => this.props.loginWithFacebook()}
+            >
+              Log in with Facebook
+            </button>
+            {this.props.login.state === 'error' && (
+              <ErrorMessage message={this.props.login.error} />
+            )}
           </div>
-          <div className="gutter" />
         </div>
-      </React.Fragment>
+        <div className="gutter" />
+      </div>
     );
   }
 }
