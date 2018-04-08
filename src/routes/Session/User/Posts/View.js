a3d63011e0515dc371c5809ace848b3c29fa1120
Johnny Oshika <johnnyoshika@gmail.com>
2018-04-08 10:47:45 -0700

    User Posts view uses WithInfinite.

--- src/routes/Session/User/Posts/View.js
+++ src/routes/Session/User/Posts/View.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import WithStatus from '../../../../components/WithStatus';
+import WithInfiniteScroll from '../../../../components/WithInfiniteScroll';
 import { selectPost } from '../../../../utils/selectors';
 import { Link } from 'react-router-dom';
 import { Image } from 'cloudinary-react';
 
-const Status = WithStatus(
+const InfiniteScroll = WithInfiniteScroll(
+  props => props.userPosts.posts.length && props.userPosts.status !== 'loading' && props.userPosts.nextUrl,
+  props => props.fetchNext(props.userPosts.id),
   props => props.userPosts.status === 'error',
   props => props.userPosts.status === 'loading'
 );
 
 class PostsView extends Component {
-  constructor(props) {
-    super(props);
-
-    // REACT ES6 classes don't autobind, so bind it in the constructor
-    // as suggested here: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md#es6-classes
-    this.onScroll = this.onScroll.bind(this);
-  }
-
   componentDidMount() {
     this.props.fetchLatest(this.props.id);
-    window.addEventListener('scroll', this.onScroll, false);
   }
 
   // had to put this here b/c when router goes from /users/1 to /user/2, new component doesn't get mounted.
@@ -28,23 +21,6 @@ class PostsView extends Component {
     if (nextProps.id !== this.props.id) {
       this.props.fetchLatest(nextProps.id);
     }
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll, false);
-  }
-
-  onScroll() {
-    if (!this.props.userPosts.posts.length) return;
-
-    if (window.innerHeight + window.scrollY < document.body.offsetHeight - 500)
-      return;
-
-    if (this.props.userPosts.status !== 'idle') return;
-    
-    if (!this.props.userPosts.nextUrl) return;
-
-    this.props.fetchNext(this.props.userPosts.id);
   }
 
   renderPost(id) {
@@ -70,11 +46,11 @@ class PostsView extends Component {
 
   render() {
     return (
-      <Status message={this.props.userPosts.error} {...this.props}>
+      <InfiniteScroll message={this.props.userPosts.error} {...this.props}>
         <div className="profile-photos">
           {this.props.userPosts.posts.map(id => this.renderPost(id))}
         </div>
-      </Status>
+      </InfiniteScroll>
     );
   }
 }
