47a144f91cb45858b1809960fb29be4ff4b124b9
Johnny Oshika <johnnyoshika@gmail.com>
2018-04-08 10:43:33 -0700

    Home view uses WithInfinite.

--- src/routes/Session/Home/View.js
+++ src/routes/Session/Home/View.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
 import Card from '../../../components/Card';
-import WithStatus from '../../../components/WithStatus';
+import WithInfiniteScroll from '../../../components/WithInfiniteScroll';
 import { selectPost } from '../../../utils/selectors';
 import './Styles.css';
 
-const Status = WithStatus(
+const InfiniteScroll = WithInfiniteScroll(
+  props =>
+    props.home.posts.length &&
+    props.home.status !== 'loading' &&
+    props.home.nextUrl,
+  props => props.fetchNext(),
   props => props.home.status === 'error',
   props => props.home.status === 'loading'
 );
 
 class HomeView extends Component {
-  constructor(props) {
-    super(props);
-
-    // REACT ES6 classes don't autobind, so bind it in the constructor
-    // as suggested here: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md#es6-classes
-    this.onScroll = this.onScroll.bind(this);
-  }
-
   componentDidMount() {
     this.props.fetchLatest();
-    window.addEventListener('scroll', this.onScroll, false);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll, false);
-  }
-
-  onScroll() {
-    if (!this.props.home.posts.length) return;
-
-    if (window.innerHeight + window.scrollY < document.body.offsetHeight - 500)
-      return;
-
-    if (this.props.home.status !== 'idle') return;
-
-    if (!this.props.home.nextUrl) return;
-
-    this.props.fetchNext();
   }
 
   render() {
@@ -45,11 +24,11 @@ class HomeView extends Component {
       <div className="home-container">
         <div className="gutter" />
         <div className="home-main">
-          <Status message={this.props.home.error} {...this.props}>
+          <InfiniteScroll message={this.props.home.error} {...this.props}>
             {this.props.home.posts.map(id => (
               <Card key={id} post={selectPost(this.props.entities, id)} />
             ))}
-          </Status>
+          </InfiniteScroll>
         </div>
         <div className="gutter" />
       </div>
